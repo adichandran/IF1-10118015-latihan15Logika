0f27e7a36b1c9d92602f02d91207dc753410731c
adichandran <55467774+adichandran@users.noreply.github.com>
2019-09-24 10:50:11 +0700

    Add files via upload

---
+++ src/if1/pkg101108015/latihan15logika/IF1101108015Latihan15logika.java
@@ -0,0 +1,30 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package if1.pkg101108015.latihan15logika;
+
+/**
+ *
+ * @author
+ * NAMA                 : Adi Chandra Nugraha
+ * KELAS                : IF-1
+ * NIM                  : 10118015
+ * Deskripsi Program    : Program Ini Berisi Program pengerjaan logika
+ */
+public class IF1101108015Latihan15logika {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        boolean a = true;
+        boolean b = false;
+        System.out.println("a && b = " +(a && b));
+        System.out.println("a || b = " +(a || b));
+        System.out.println("!a = " +(!a));
+        System.out.println("!(a && b) = " + !(a && b));
+    }
+    
+}
